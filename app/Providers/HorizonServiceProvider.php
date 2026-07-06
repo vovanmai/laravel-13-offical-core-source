8a45fe2ccdc3a754ba67056f398fc164ba8d7e1c
TechTus Vo Van Mai <maivv@mynavitechtus.com>
2026-07-06 15:39:38 +0700

    refactore

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,9 +28,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                1
-            ]);
+            return true;
         });
     }
 }
