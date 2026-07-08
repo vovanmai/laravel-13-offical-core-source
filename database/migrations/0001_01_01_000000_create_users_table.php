cb25fd54a6b7467745c3926da484c8191135d6df
TechTus Vo Van Mai <maivv@mynavitechtus.com>
2026-07-08 21:07:12 +0700

    add

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedTinyInteger('status')->default(UserStatus::ACTIVE->value);
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('password_changed_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
